8d9b047c29e6a32a58bccd66828b5902606f6aab
xenxynon <flynryder427@gmail.com>
2022-08-08 09:23:16 +0000

    chime : declare board platform

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/xiaomi/chime/chime-vendor.mk)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Board
+TARGET_BOARD_PLATFORM := sdm662
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
